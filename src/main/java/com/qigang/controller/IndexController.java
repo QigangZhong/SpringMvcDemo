997c3c865b706f65ae10d3be7183fd29953ea64a
zhongqigang <zhongqigang@benlai.com>
2017-09-28 19:10:03 +0800

    change mvc version

--- src/main/java/com/qigang/controller/IndexController.java
+++ src/main/java/com/qigang/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.qigang.controller;
 
 import com.qigang.domain.ResponseData;
+import com.sun.org.apache.xpath.internal.SourceTree;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,12 +21,13 @@ public class IndexController {
 
     @RequestMapping(value="/token")
     @ResponseBody
-    public ResponseData token(String token){
+    public ResponseData token(String code, String token){
         ResponseData data=new ResponseData();
         data.setCode("0");
         data.setMsg("success");
         data.setData("");
-        System.out.println(token);
+        System.out.println("code: "+code);
+        System.out.println("token: "+token);
         //如果需要返回restful风格json数据, pom.xml中需要添加jackson的2个包,并且打@ResponseBody
         return data;
     }
